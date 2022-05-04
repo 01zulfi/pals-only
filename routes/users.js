e03f57412fd64c9bb43e9411e81a8a2361934b0a
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-04 16:37:17 +0500

    Assign controllers for get/post requests in users route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const controller = require('../controllers/users-controller');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
+router.get('/signup', controller.signupGet);
+router.post('/signup', controller.signupPost);
+router.get('/login', controller.loginGet);
+router.post('/login', controller.loginPost);
+router.get('/logout', controller.logout);
+router.get('/:id', controller.userDetail);
 
 module.exports = router;
