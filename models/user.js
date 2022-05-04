60a41ae14366922e57d9b223c45ac94e27c36f5a
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-04 14:48:59 +0500

    Add user.js model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  isMember: { type: Boolean },
+  isAdmin: { type: Boolean },
+});
+
+module.exports = mongoose.model('User', UserSchema);
