55d7d0ef2e95ce383249e308ebd03e05ef4b001c
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-04 17:34:15 +0500

    Add signupPost middleware function

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 const User = require('../models/user');
 
 passport.use(
@@ -35,3 +36,41 @@ exports.signupGet = (req, res, next) => {
   if (req.user) return res.redirect('/');
   return res.render('signup-form');
 };
+
+exports.signupPost = [
+  body('username', 'Username must not be empty').trim().notEmpty().escape(),
+  body('password', 'Password must not be empty').trim().notEmpty().escape(),
+  body('confirmPassword', 'Password must match with confirm password field')
+    .trim()
+    .escape()
+    .custom((value, { req }) => value === req.body.password),
+  body('firstName').trim().escape(),
+  body('lastName').trim().escape(),
+  (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+      if (err) return next(err);
+      const errors = validationResult(req);
+
+      const user = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: hashedPassword,
+      });
+
+      if (!errors.isEmpty()) {
+        return res.render('signup-form', {
+          ...user,
+          errors: errors.array(),
+        });
+      }
+
+      return user.save((error) => {
+        if (error) {
+          return next(error);
+        }
+        return res.redirect('/');
+      });
+    });
+  },
+];
