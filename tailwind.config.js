377759d1481ed1503ede39e82f12cac850149483
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-05-05 17:20:55 +0500

    Add colors & font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./views/*.pug'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'd-bg': '#282a36',
+        'd-cl': '#44475a',
+        'd-fg': '#f8f8f2',
+        'd-comment': '#6272a4',
+        'd-cyan': '#8be9fd',
+        'd-green': '#50fa7b',
+        'd-orange': '#ffb86c',
+        'd-pink': '#ff79c6',
+        'd-purple': '#bd93f9',
+        'd-Red': '#ff5555',
+        'd-yellow': '#f1fa8c',
+      },
+    },
   },
   plugins: [],
 };
